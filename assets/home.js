2f8883cd35dd21ea5ebb0c183b288c369b688b17
unknown <tomalhossen78@gmail.com>
2025-08-22 23:42:31 +0600

    3 sections done finaliy

--- assets/home.js
+++ assets/home.js
@@ -1,4 +1,29 @@
+const validPin = 1234;
 // capture add money btn
+
 document.getElementById('btn-add-money').addEventListener('click',function(even){
     event.preventDefault();
+    // capture bank input
+    const bank = document.getElementById('select-bank').value;
+    const accountNumber =parseInt(document.getElementById('bank-account-number').value);
+    const addAmount = parseInt(document.getElementById('add-amount').value);
+    const pin = parseInt(document.getElementById('pin-num').value);
+    console.log(bank, accountNumber, addAmount, pin);
+    const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+    
+    if(toString(accountNumber).length<11){
+        
+        alert('Please the valid Account Number')
+        return;
+    }
+    if(pin!=validPin){
+        alert('Please Enter the Valid Pin');
+        return;
+    }
+    const totalBalance = availableBalance + addAmount;
+    document.getElementById('available-balance').innerText=totalBalance;
+    
+
+
+
 })
