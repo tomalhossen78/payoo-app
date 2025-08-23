e23e1068831cdd86ef63c5c14da7c2bc226e4859
unknown <tomalhossen78@gmail.com>
2025-08-23 10:48:04 +0600

    cashout feature done

--- assets/home.js
+++ assets/home.js
@@ -5,13 +5,15 @@ document.getElementById('btn-add-money').addEventListener('click',function(even)
     event.preventDefault();
     // capture bank input
     const bank = document.getElementById('select-bank').value;
-    const accountNumber =parseInt(document.getElementById('bank-account-number').value);
+    const accountNumber =document.getElementById('bank-account-number').value;
     const addAmount = parseInt(document.getElementById('add-amount').value);
     const pin = parseInt(document.getElementById('pin-num').value);
     console.log(bank, accountNumber, addAmount, pin);
     const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+
     
-    if(toString(accountNumber).length<11){
+    
+    if(accountNumber.length!==11){
         
         alert('Please the valid Account Number')
         return;
@@ -25,5 +27,51 @@ document.getElementById('btn-add-money').addEventListener('click',function(even)
     
 
 
+})
+
+// logout button functionality
+
+document.getElementById('btn-log-out').addEventListener('click',function(){
+     window.location.href="./index.html"
+     console.log('logout btn clicked')
+})
+
+// add money feature functionality
+
+document.getElementById('add-money').addEventListener('click',function(){
+    document.getElementById('add-money-section').classList.remove('hidden');
+
+    document.getElementById('cash-out-section').classList.add('hidden');
+})
+
+// cashout feature functionality
+document.getElementById('cash-out').addEventListener('click',function(){
+    document.getElementById('cash-out-section').classList.remove('hidden');
+
+    document.getElementById('add-money-section').classList.add('hidden');
+})
+
+// cashout button fuctionality
+
+document.getElementById('btn-withdraw-money').addEventListener('click',function(event){
+    event.preventDefault();
+   const withdrawAmount = parseInt(document.getElementById('withdraw-amount').value);
+   const availableBalance = parseInt(document.getElementById('available-balance').innerText)
+   const agentNumber = document.getElementById('agent-number').value;
+   const pin = parseInt(document.getElementById('pin-num-agent').value);
+    
+    
+    if(agentNumber.length!==11){
+        
+        alert('Please the valid Agent Number')
+        return;
+    }
+    if(pin!=validPin){
+        alert('Please Enter the Valid Pin');
+        return;
+    }
+    const totalBalance = availableBalance - withdrawAmount;
+    document.getElementById('available-balance').innerText=totalBalance;
 
 })
+
